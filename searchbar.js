6d234038222aa1fcce0bbe1a0fe5c898d1cc458d
Kaitlyn <kaitlynkhan@aol.com>
2022-03-09 15:47:25 -0500

    updating API code

--- searchbar.js
+++ searchbar.js
@@ -1,21 +1,28 @@
-fetch("https://50k-radio-stations.p.rapidapi.com/get/channels?keyword=a&country_id=50&page=1", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "50k-radio-stations.p.rapidapi.com",
-		"x-rapidapi-key": "12243b1d97mshde8e8d53591ba1fp1c3c6ejsn59205afc2c46"
-	}
-})
-.then(response => response.json())
-.then(data => {
-    const list = data.d;
-
-    list.map(item) => {
+fetchRadioStations = () => {
+    fetch("https://50k-radio-stations.p.rapidapi.com/get/channels?keyword=a&country_id=50&page=1", {
+	    "method": "GET",
+	    "headers": {
+	    	"x-rapidapi-host": "50k-radio-stations.p.rapidapi.com",
+		    "x-rapidapi-key": "12243b1d97mshde8e8d53591ba1fp1c3c6ejsn59205afc2c46"
+	    }
+    })
+    .then(response => response.json()) //getting the actual response data
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        HTMLFormControlsCollection.log(err);
+    });
+    /*list.map(item) => {
         const name = item.l;
-        const poster = item.i.imageUrl;
+        //const poster = item.i.imageUrl;
         const station = '<li><img src="${poster}" <h2>${name}</h2><li>'
         document.querySelector('.stations').innerHTML += station;
-    })
-})
-.catch(err => {
-	console.error(err);
-});
+    })*/
+}
+componentDidMount(){
+    this.fetchRadioStations();
+}
+/*.catch(err => {
+	console.error(err); 
+});*/
